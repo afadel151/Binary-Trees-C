a42b911d42a0a024d3f02db8d108b3e2b51fcfe3
Rajeev Tapadia <rtapadia730@gmail.com>
2023-08-07 20:02:53 +0530

    searchBST and inorder successor

--- BST.c
+++ BST.c
@@ -2,9 +2,14 @@
 #include "Headers/Binary Tree.h"
 
 // to be done
-// create bst
-// bst insertion
 // bst deletion 3 cases
+/*
+    NO CHILD: set ancesotrs to point NULL 
+                and free node
+    SINGLE CHILD: set ancestor->next = child
+                and free node
+    TWO CHILD: inorder predecessor or inorder successor
+*/ 
 
 struct node* insertBST(struct node* root, int data){
     if(root == NULL){
@@ -41,6 +46,38 @@ struct node* createBST(int* arr, int count){
     return root;
 }
 
+struct node* searchBST(struct node* root, int key){
+    if(root == NULL)
+        return root;
+    while (root != NULL){
+        if(key < root->data)
+            root = root->left;
+        else if(key > root->data)
+            root = root->right;
+        else if(key == root->data)
+            return root;
+    }
+    printf("Node not found\n");
+    return root;
+}
+
+struct node* inorderSucc(struct node* root){
+    // finding the leftmost child of the right subtree
+    // do not use for a tree with only one element
+    if(root->left == NULL)
+        return NULL;
+    struct node* succ, *succParent;
+    succ = root->left;
+    succParent = root;
+    while(succ->left != NULL){
+        succParent = succParent->left;
+        succ = succ->left;
+    }
+    // printf("succParent: %d\nsucc: %d", succParent->data, succ->data);
+    return succ;
+}
+
+
 void main(){
     int arr[] = {4,2,1,3,6,5,7};
     int size = sizeof(arr)/sizeof(int);
@@ -48,4 +85,9 @@ void main(){
     struct node* root = createBST(arr, size);
     
     printLevelWise(root);
+
+    struct node* thrash = inorderSucc(root);
+    struct node* res = searchBST(root, arr[0]);
+    if( res->data == arr[0])
+        printf("pass\n");
 }
